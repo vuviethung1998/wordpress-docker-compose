cd0d9cf774e3f032adc1a64171eceab512328bb2
hungvv <hungvv@cenland.vn>
2020-01-31 18:00:16 +0700

    update

--- wp-app/wp-config.php
+++ wp-app/wp-config.php
@@ -23,13 +23,13 @@
 define( 'DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define( 'DB_USER', 'root');
+define( 'DB_USER', 'wordpress');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password');
+define( 'DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define( 'DB_HOST', 'db');
+define( 'DB_HOST', 'db:3306');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8');
